92d80f8c6caf3dd821dd7b6be8af2b1198cfd5a7
Luiz Gustavo Falqueto <lgfalqueto85@gmail.com>
2021-09-03 21:33:55 -0300

    Meu segundo commit

---
+++ database/migrations/2021_09_03_233033_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title',255);
+            $table->string('genre',255);
+            $table->string('country',255);
+            $table->string('release',255);
+            $table->text('synopsis',1000); 
+            $table->string('rating',255);
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Movies | Adapti</title>
 </head>
 <body>
-
+    @foreach ($movies as $movie)
+        <h4>{{ $movie->title }}</h4>
+        <img src="{{$movie->image}}" alt="Imagem">
+    @endforeach
 </body>
 </html>
